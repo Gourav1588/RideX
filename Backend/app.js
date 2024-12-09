bbbe541db15d5ec12f83cb2614e1da89c412cba5
unknown <you@exam>
2024-12-09 19:13:49 +0530

    all

--- Backend/app.js
+++ Backend/app.js
@@ -3,6 +3,7 @@ dotnev.config();
 const express=require("express")
 const cors=require("cors")
 const app=express()
+const main=require('./db/db')
 
 app.use(cors())
 
---
+++ Backend/db/db.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+
+
+     mongoose.connect('mongodb://127.0.0.1:27017/test');
+    console.log("voneec")
+  
+    // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+
