9e95a82fdb15162c5a1fc81ad431e83ad02dbf8d
unknown <you@exam>
2025-04-18 15:25:42 +0530

    frontend change

--- Backend/app.js
+++ Backend/app.js
@@ -1,25 +1,40 @@
-const dotnev=require('dotenv')
-dotnev.config();
-const express=require("express")
-const cors=require("cors")
-const app=express()
-const main=require('./db/db')
-app.use(express.json())
-const userRoute=require('./routes/user.routes')
-const captainroutes=require("./routes/captain.routes")
-const cookieParser = require('cookie-parser');
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Routes
+const userRoute = require("./routes/user.routes");
+const captainroutes = require("./routes/captain.routes");
+const mapRoutes = require("./routes/maps.routes");
+const cookieParser = require("cookie-parser");
+
 app.use(cookieParser());
-const mapRoutes=require('./routes/maps.routes')
+app.use("/users", userRoute);
+app.use("/captains", captainroutes);
+app.use("/maps", mapRoutes);
+require('dotenv').config();  // This loads the .env file
+
+const mongoose = require('mongoose');
+
+// Use the MONGO_URI from the environment variables
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("MongoDB connection error:", err));
 
 
-app.use(cors())
+// Test route
+app.get("/", (req, res) => {
+  res.send("hello");
+});
 
-app.get('/',(req,res)=>{
-    res.send('hello')
-})
-app.use('/users',userRoute)
-app.use('/captains',captainroutes)
-app.use('/maps', mapRoutes);
+// The `app.listen` part that starts the server
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
 
-
-module.exports =app;
+module.exports = app;
